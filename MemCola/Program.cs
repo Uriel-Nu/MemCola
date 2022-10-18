5140abc91b18e083f2d7629d29557df0de3c8258
UsuarioUsuario@DESKTOP-2NT6HAM <urielalberto75@hotmail.com>
2022-10-18 15:09:25 -0300

    Agregar archivos de proyecto.

---
+++ MemCola/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemCola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // cola : Estructura FIFO ( primero en entrar, primero en salir
+            {
+                string cadena;
+                Queue colaStr = new Queue();
+                Console.WriteLine("Ingrese una cadena en la cola");
+                cadena = Console.ReadLine();
+                while (cadena != string.Empty)
+                {
+                    colaStr.Enqueue(cadena);
+                    Console.WriteLine("Ingrese próxima cadena en la cola");
+                    cadena = Console.ReadLine();
+                }
+                while (colaStr.Count > 0)
+                {
+                    Console.WriteLine((string)colaStr.Dequeue());
+                }
+                Console.ReadKey();
+            }
+        }
+
+
+    }
+    }
